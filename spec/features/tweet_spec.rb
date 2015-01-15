1b57edcfa30c31a72258ce1f9f048142bba118ff
godspeedyoo & Paul <paulyu292@gmail.com>
2015-01-15 11:20:33 -0800

    rspec /tweets route test

---
+++ spec/features/tweet_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../spec_helper'
+
+describe "Tweet shit" do
+  describe "get /tweets" do
+    it "displays tweets" do
+      get '/tweets'
+      expect(last_response.status).to eq(200)
+    end
+  end
+
+end
