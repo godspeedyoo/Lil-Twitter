7b6633aaa732117781c93d7b5277148dccaca807
godspeedyoo & Paul <paulyu292@gmail.com>
2015-01-15 11:45:12 -0800

    pass rspec: post /tweets

--- app/controllers/tweet.rb
+++ app/controllers/tweet.rb
@@ -2,3 +2,9 @@ get '/tweets' do
   @tweets = Tweet.all
   erb :'tweets/index'
 end
+
+post '/tweets' do
+  p params
+  @user = User.create(username: "One")
+  @tweet = @user.tweets.create(params)
+end
